c9b8a3cb8384d539ab538d54faafa3ec1af4937e
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-10-20 19:06:54 +0300

    test: make them pass

--- tests/HttpMethodTest.php
+++ tests/HttpMethodTest.php
@@ -6,7 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-namespace Tests;
+namespace BrokeYourBike\HttpEnums\Tests;
 
 use PHPUnit\Framework\TestCase;
 use BrokeYourBike\HttpEnums\HttpMethodEnum;
